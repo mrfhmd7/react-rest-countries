4ff56e55d1008a23a4d7993282ba172c984e6fd5
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-24 14:17:34 +0600

    get countries data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -11,10 +11,16 @@ function App() {
 }
 
 function LoadCountries() {
-  const [country, setCountry] = useState([])
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  }, []);
   return (
     <div>
       <h1>Visiting Every Countries of the World!!</h1>
+      <h3>Available Countries: {countries.length}</h3>
     </div>
   );
 };
