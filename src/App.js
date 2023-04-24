22b3d12d2f72099da67180855afe50c1cd1fbdf1
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-24 15:02:32 +0600

    components file added

--- src/App.js
+++ src/App.js
@@ -21,8 +21,20 @@ function LoadCountries() {
     <div>
       <h1>Visiting Every Countries of the World!!</h1>
       <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
     </div>
   );
 };
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  );
+}
 
 export default App;
